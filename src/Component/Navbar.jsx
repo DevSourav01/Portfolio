79c0aba58fd6250c7527ea86b68e38f53f944118
Sourav Nandi <sn9378115@gmail.com>
2025-03-20 21:08:41 +0530

    'changes'

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   };
 
   return (
-    <div className="fixed top-5 left-1/2 transform -translate-x-1/2 w-[70vw] max-w-[500px] min-w-[280px] flex flex-wrap items-center justify-center gap-6 px-6 py-3 bg-gray-900 border border-cyan-400 shadow-lg rounded-full z-50 transition-all duration-300 animate-glow">
+    <div className="fixed top-5 left-1/2 transform -translate-x-1/2 w-[70vw] max-w-[500px] min-w-[280px] flex  items-center justify-center gap-3 px-6 py-3 bg-gray-900 border border-cyan-400 shadow-lg rounded-full z-50 transition-all duration-300 animate-glow ">
       {navItems.map((item) => (
         <p
           key={item}
